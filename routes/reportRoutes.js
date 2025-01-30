d04b4bba0d3a40dd6a3c787987b230e838af0781
HawaMuhumedAli <luulh32@gmail.com>
2025-01-30 06:43:35 -0800

    Updated project with latest changes

--- routes/reportRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getReport } = require('../controllers/reportController');
-const checkAdmin = require('../middleware/checkAdmin');
-
-const router = express.Router();
-
-router.get('/report', checkAdmin, getReport);
-
-module.exports = router;
